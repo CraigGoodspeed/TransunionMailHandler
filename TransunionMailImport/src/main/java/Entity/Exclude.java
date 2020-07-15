dd4db08110a04480c79bbecbcef5e45cafea301d
krik <craig@goodspeed.co.za>
2020-07-15 21:31:42 +0200

    code commit, import all needed items.

--- TransunionMailImport/src/main/java/Entity/Exclude.java
+++ TransunionMailImport/src/main/java/Entity/Exclude.java
@@ -5,7 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
+/**
+ *The attribute is used so when we handle items from the csv file we can create temporary variables that will not be imported
+ * Exclude ie - dont import me.
+ */
 public @interface Exclude {
 }
