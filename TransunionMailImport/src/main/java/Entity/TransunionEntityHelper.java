d8084dde894feb9b7722859dacd54376c0ee717d
krik <craig@goodspeed.co.za>
2020-07-15 23:02:24 +0200

    code commit, import all needed items.

--- TransunionMailImport/src/main/java/Entity/TransunionEntityHelper.java
+++ TransunionMailImport/src/main/java/Entity/TransunionEntityHelper.java
@@ -4,9 +4,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class TransunionEntityHelper{
