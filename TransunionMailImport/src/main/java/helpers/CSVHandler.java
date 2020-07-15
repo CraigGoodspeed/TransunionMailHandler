4943fadc43049e662389571e15cd8e540a1158c9
krik <craig@goodspeed.co.za>
2020-07-15 10:26:20 +0200

    code commit, import all needed items.

--- TransunionMailImport/src/main/java/helpers/CSVHandler.java
+++ TransunionMailImport/src/main/java/helpers/CSVHandler.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 public class CSVHandler {
-    int batchSize = 100;
+    int batchSize;
     public CSVHandler(int batchSize){
         this.batchSize= batchSize;
     }
